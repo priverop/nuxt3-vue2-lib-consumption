65901c1f7cfa3bf922c45d6e3e2a5c38018da28b
Pablo Rivero <priverop>
2023-02-02 13:05:52 +0100

    refactor: clean nuxt 3 project

--- nuxt3-lib-consumer/app.vue
+++ nuxt3-lib-consumer/app.vue
@@ -1,12 +1,5 @@
-<script setup>
-  import {LibraryComponent} from '@dcrall/vite-test-lib'
-</script>
 <template>
   <div>
-    <h1>Nuxt 3 app</h1>
-
-    <LibraryComponent>
-      Consuming library
-    </LibraryComponent>
+    <NuxtWelcome />
   </div>
 </template>
